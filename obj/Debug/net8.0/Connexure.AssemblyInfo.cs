73af0bce11989f28b9cb5a00b2c52f6c3ed921fc
Nilesh Pawar <nilesh.pawar@bizsense.in>
2025-02-22 23:45:48 +0530

    #main: "Homepage Title to blank"

--- obj/Debug/net8.0/Connexure.AssemblyInfo.cs
+++ obj/Debug/net8.0/Connexure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Connexure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d45b345eb62256f40e698647ff3610213b610519")]
 [assembly: System.Reflection.AssemblyProductAttribute("Connexure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Connexure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
